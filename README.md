848a37f2f6366e4f85c663b35e89a9aa625ebeea
Cythonic1 <jfjbn80@gmail.com>
2025-01-22 18:45:15 +0800

    Init the packet sniffer

---
+++ README.md
@@ -0,0 +1,8 @@
+# This is a packet Sniffer and basic IDS/IPS Written in C. Becuase C is cool :)
+
+```bash
+to run the code use the bash script to run it easily
+
+chmod u+x compile.sh 
+./compile.sh
+```
---
+++ libpcapFun.c
@@ -0,0 +1,125 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <pcap.h>
+#include <pcap/pcap.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <net/ethernet.h>
+
+// Function to print the LinkedList
+void printLinkedList(pcap_if_t *list ){
+
+    pcap_if_t *next_p;
+    next_p = list;
+    while (next_p != NULL){
+        fprintf(stdout, "The name of the interface is : %s\n", next_p->name);
+        next_p = next_p->next;
+
+    }
+}
+
+
+void printPacketHeader(const u_char *packet, struct pcap_pkthdr packetHeader){
+    fprintf(stdout, "Packet Capture Length %d\n",packetHeader.caplen);
+    fprintf(stdout, "Packet Total Length %d\n",packetHeader.len);
+}
+
+void packetHandler(u_char  *args, const struct pcap_pkthdr *packetHeader, const u_char *packetBody){
+    struct ether_header *etherHeader;
+    etherHeader = (struct ether_header *) packetBody;
+    int etherHeaderType = ntohs(etherHeader->ether_type);
+    if(etherHeaderType == ETHERTYPE_IP){
+        fprintf(stdout, "This packet is an IP packet\n");
+        fprintf(stdout, "------------------------------------\n");
+    }else if(etherHeaderType == ETHERTYPE_ARP){
+        fprintf(stdout, "This packet is an ARP packet\n");
+        fprintf(stdout, "------------------------------------\n");
+    }else if(etherHeaderType == ETHERTYPE_REVARP){
+        fprintf(stdout, "This packet is a Reverse ARP  packet\n");
+        fprintf(stdout, "------------------------------------\n");
+    }
+    /*printPacketHeader(packetBody, *packetHeader);*/
+    return ;
+}
+int main(int argc , char **argv){
+    // Gonna hold the list of devices
+    pcap_if_t *devicesList;
+
+    // Buffer to hold the error value
+    char errorBuffer[PCAP_ERRBUF_SIZE];
+
+
+
+    if(pcap_findalldevs(&devicesList, errorBuffer) == PCAP_ERROR){
+        fprintf(stderr, "Not able to find the NICs");
+        return 1;
+    }
+    printLinkedList(devicesList);
+
+    // **Getting more info about the network device**;
+    char ip[16] = {0};
+    char subNetMask[16] = {0};
+    bpf_u_int32 ip_address; // To hold the IP address as an integer
+    bpf_u_int32 subNetMask_raw; // Same as the above but for the submask
+    int lookUpReturnCode;
+    struct in_addr address;
+    char *deviceName = devicesList->name; // devices list will point at the first device which is the device i want
+
+    lookUpReturnCode = pcap_lookupnet(deviceName, &ip_address, &subNetMask_raw,  errorBuffer);
+    if(lookUpReturnCode == -1){
+        fprintf(stderr, "Error while getting devices info\n");
+        fprintf(stderr, "%s\n", errorBuffer);
+        return 1;
+    }
+
+    address.s_addr = ip_address;
+    strncpy(ip, inet_ntoa(address), 15);
+    ip[15] = '\0';
+    if(ip[0] == '\0'){
+        fprintf(stderr, "Error in inet_ntoa\n");
+        return 1;
+    }
+
+
+    address.s_addr = subNetMask_raw;
+    strncpy(subNetMask, inet_ntoa(address), 15);
+    subNetMask[15] = '\0';
+    if(subNetMask[0] == '\0'){
+        fprintf(stderr, "Error in inet_ntoa\n");
+        return 1;
+    }
+
+    fprintf(stdout, "The device name is %s\n", deviceName);
+    fprintf(stdout, "The device ip address is %s\n", ip);
+    fprintf(stdout, "The device subnet mask is %s\n", subNetMask);
+
+    /*pcap_freealldevs(devicesList);*/
+
+    // ** Capture Live packets with the interface we have ** 
+
+    pcap_t *handler;
+    const u_char *packet;
+    struct pcap_pkthdr packetHeader;
+    int packetCountLimit = 1;
+    int packetTimeOut = 90000; // In milliseconds
+
+    handler = pcap_open_live(
+        deviceName,
+        BUFSIZ,
+        packetCountLimit,
+        packetTimeOut,
+        errorBuffer
+    );
+
+    // This will attept to listen for a single packet at a time if the timeout passes it will return null.
+    pcap_loop(handler, 0, packetHandler, NULL);
+    /*if(packet == NULL){*/
+    /*    fprintf(stderr, "No packet Found\n");*/
+    /*    return 1;*/
+    /*}*/
+
+    printPacketHeader(packet, packetHeader);
+
+    return 0;
+}

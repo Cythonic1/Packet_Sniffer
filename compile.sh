62d2db72e89d0c88942a6ded4db056be578ee0ff
Cythonic1 <jfjbn80@gmail.com>
2025-03-01 02:30:41 +0800

    adding more Clang flags and task

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-clang ./src/main.c -lpcap -Wall -Wextra -o ./main
+clang ./src/main.c -lpcap -Wall -Wextra -fsanitize=address -fsanitize=undefined -fstack-protector-strong -D_FORTIFY_SOURCE=2 -fPIE -pie -z relro -z now -o ./main
 
 if [ $? -eq 0 ];then
     sudo ./main
